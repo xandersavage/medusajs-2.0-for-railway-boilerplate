7d445894ed7e1fee42b8dd4365e047c9a5bfaf7b
Alexander <swankylex@gmail.com>
2025-01-04 01:01:35 +0100

    changed some medusa references to pawklan

--- storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -57,10 +57,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   return {
-    title: `${product.title} | Medusa Store`,
+    title: `${product.title} | PawKlan Store`,
     description: `${product.title}`,
     openGraph: {
-      title: `${product.title} | Medusa Store`,
+      title: `${product.title} | PawKlan Store`,
       description: `${product.title}`,
       images: product.thumbnail ? [product.thumbnail] : [],
     },
